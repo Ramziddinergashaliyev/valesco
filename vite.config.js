090847959cb02002872529feb9390d11d2347eed
Ramziddinergashaliyev <155714547+Ramziddinergashaliyev@users.noreply.github.com>
2026-06-29 17:02:55 +0500

    add change hero page

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,11 @@ export default defineConfig({
         changeOrigin: true,
         secure: true,
       },
+      '/sitemap.xml': {
+        target: 'https://backend.valescooil.com',
+        changeOrigin: true,
+        secure: true,
+      },
     },
   },
 })
