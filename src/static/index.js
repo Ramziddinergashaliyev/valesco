2c5ba7cc3841f34509e5b07adb5d9805407b4980
Ramziddinergashaliyev <155714547+Ramziddinergashaliyev@users.noreply.github.com>
2026-09-14 11:17:30 +0500

    dd

--- src/static/index.js
+++ src/static/index.js
@@ -591,21 +591,21 @@ export const newsData = [
     {
         id: 6,
         category: "Новости",
-        world: ["MIMS Automobility Petersburg", "Automechanika Astana"],
+        world: ["Automechanika Astana"],
         title: "Automechanika 2026",
         description: ["Valesco продолжает свой глобальный путь, принимая участие в ведущих международных автомобильных выставках 2026 года. Эти мероприятия предоставляют платформу для демонстрации наших высококачественных смазочных материалов, налаживания связей с профессионалами индустрии и изучения новых бизнес-возможностей по всему миру.", "Благодаря активному участию в глобальных выставках, Valesco укрепляет свое присутствие на рынке послепродажного обслуживания автомобилей и подтверждает свою приверженность инновациям, качеству и надежной работе.", "Эти выставки отражают стремление Valesco к созданию прочных партнерских отношений и предоставлению надежных решений в области смазочных материалов для мировых рынков."],
         date: "2026",
         img: news3,
-        image: [mimsOne, main26],
+        image: [main26],
         locationData: [
-            {
-                id: 'peter',
-                title: 'Valesco на MIMS Automobility Санкт-Петербург 2026',
-                location: 'Выставочный центр Экспофорум, Санкт-Петербург',
-                description: 'Компания Valesco приняла участие в выставке MIMS Санкт-Петербург, которая прошла в выставочном центре Экспофорум, представив на своём стенде G035 в павильоне G полный ассортимент автомобильных смазочных материалов и технических жидкостей. Выставка предоставила возможность наладить контакты с партнёрами и дистрибьюторами, представить новейшие разработки бренда и укрепить присутствие Valesco на автомобильном рынке Санкт-Петербурга. Команда с нетерпением ждёт дальнейшего участия в предстоящих отраслевых мероприятиях по мере расширения присутствия Valesco на новых рынках. Valesco — надёжная защита. Мощная производительность.',
-                images: [mimsTwo, mimsThree, mimsFour, mimsFife],
-                position: 'left'
-            },
+            // {
+            //     id: 'peter',
+            //     title: 'Valesco на MIMS Automobility Санкт-Петербург 2026',
+            //     location: 'Выставочный центр Экспофорум, Санкт-Петербург',
+            //     description: 'Компания Valesco приняла участие в выставке MIMS Санкт-Петербург, которая прошла в выставочном центре Экспофорум, представив на своём стенде G035 в павильоне G полный ассортимент автомобильных смазочных материалов и технических жидкостей. Выставка предоставила возможность наладить контакты с партнёрами и дистрибьюторами, представить новейшие разработки бренда и укрепить присутствие Valesco на автомобильном рынке Санкт-Петербурга. Команда с нетерпением ждёт дальнейшего участия в предстоящих отраслевых мероприятиях по мере расширения присутствия Valesco на новых рынках. Valesco — надёжная защита. Мощная производительность.',
+            //     images: [mimsTwo, mimsThree, mimsFour, mimsFife],
+            //     position: 'left'
+            // },
             {
                 id: 'astana',
                 title: 'Automechanika Astana',
@@ -856,21 +856,21 @@ export const newsDataEn = [
     {
         id: 6,
         category: "News",
-        world: ["MIMS Automobility Petersburg", "Automechanika Astana"],
+        world: ["Automechanika Astana"],
         title: "Automechanika 2026",
         description: ["Valesco continues its global journey by participating in leading international automotive exhibitions in 2026. These events provide a platform to showcase our high-quality lubricants, connect with industry professionals, and explore new business opportunities worldwide.", "Through active participation in global exhibitions, Valesco strengthens its presence in the automotive aftermarket and reinforces its commitment to innovation, quality, and reliable performance.", "These exhibitions reflect Valesco’s vision of building strong partnerships and delivering trusted lubrication solutions to global markets."],
         date: "2026",
         img: news3,
-        image: [mimsOne, main26],
+        image: [main26],
         locationData: [
-            {
-                id: 'peter',
-                title: 'Valesco at MIMS Automobility Saint Petersburg 2026',
-                location: 'Expoforum Exhibition Centre Saint Petersburg',
-                description: 'Valesco took part in MIMS Saint Petersburg, held at Expoforum Exhibition Centre, showcasing its full range of automotive lubricants and fluids at Pavilion G, Stand G035. The exhibition offered an opportunity to connect with partners and distributors, present the brand\'s latest product developments, and strengthen Valesco\'s presence in the Saint Petersburg automotive market. The team looks forward to continued participation in upcoming industry events as Valesco expands its reach across new markets. Valesco — Trusted Protection.Powerful Performance.',
-                images: [mimsTwo, mimsThree, mimsFour, mimsFife],
-                position: 'right'
-            },
+            // {
+            //     id: 'peter',
+            //     title: 'Valesco at MIMS Automobility Saint Petersburg 2026',
+            //     location: 'Expoforum Exhibition Centre Saint Petersburg',
+            //     description: 'Valesco took part in MIMS Saint Petersburg, held at Expoforum Exhibition Centre, showcasing its full range of automotive lubricants and fluids at Pavilion G, Stand G035. The exhibition offered an opportunity to connect with partners and distributors, present the brand\'s latest product developments, and strengthen Valesco\'s presence in the Saint Petersburg automotive market. The team looks forward to continued participation in upcoming industry events as Valesco expands its reach across new markets. Valesco — Trusted Protection.Powerful Performance.',
+            //     images: [mimsTwo, mimsThree, mimsFour, mimsFife],
+            //     position: 'right'
+            // },
             {
                 id: 'astana',
                 title: 'Automechanika Astana',
